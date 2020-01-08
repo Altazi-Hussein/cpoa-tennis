ea7f4eddaa227b12806012b941ad613e09cfb190
Angelo Al Yacoub <angelo.al-yacoub@etu.univ-lyon1.fr>
2020-01-08 23:11:17 +0100

    package tennis contenant un main pour tester le lien avec MariaDB ajouté

---
+++ tennis/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tennis;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.ConnexionBD;
+import model.DAO.JoueurDAO;
+import planning.Joueur;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author angel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        try {
+            // TODO code application logic here
+            final Connection conn = ConnexionBD.getConnection();
+            JoueurDAO joueurDAO = new JoueurDAO(conn);
+            List<Joueur> lesJoueurs = joueurDAO.findAll();
+            System.out.println(lesJoueurs.get(0).getIdJoueur());
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
